b0e7314a02fa633e0a933ceb8360ad93b7de9a29
hhvergara <hector.vergara@entreconsultas.com>
2021-08-10 02:44:27 -0300

    Agregamos los archivos de tarea

---
+++ ejemplos_clase/marvel/e_commerce/api/serializers.py
@@ -0,0 +1,26 @@
+
+# Primero importamos los modelos que queremos serializar:
+from e_commerce.models import Comic,WishList
+from django.contrib.auth.models import User
+
+# Luego importamos todos los serializadores de django rest framework.
+from rest_framework import serializers
+
+class ComicSerializer(serializers.ModelSerializer):
+    # algo =  serializers.SerializerMethodField()
+    
+    class Meta:
+        model = Comic
+        fields = ('marvel_id','title', 'description', 'price', 'stock_qty', 'picture')
+        # fields = ('marvel_id', 'title', 'algo')
+
+    # def get_algo(self, obj):
+    #     return {'hola':10}
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields= ("__all__")
+
+# TODO: Realizar el serializador para el modelo de WishList
